dd4b7ac88130e52b0c6f5ec8b0ac251b303f1650
Monika <monika.ashcraft@gmail.com>
2021-11-30 20:35:44 +0100

    sdfjaghasoegbfjvhisue kuhdt

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { pageNames } from "./data/pages";
 import { Page } from "./page/Page";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 export const App = () => {
   return (
-    <Router basename="ihateslp">
+    <Router>
       <Routes>
-        <Route path="/" element={<Page page="default" />} />
         {pageNames.map((name) => (
           <Route key={name} path={name} element={<Page page={name} />} />
         ))}
+        <Route path="/" element={<Page page="default" />} />
       </Routes>
     </Router>
   );
